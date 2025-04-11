86002f9b4c440aa70da43b2c6052e540a01020ac
Simas <simaszurauskas@gmail.com>
2025-04-11 09:44:28 +0300

    dev

--- src/lib/ai/embeddings.ts
+++ src/lib/ai/embeddings.ts
@@ -1,6 +1,6 @@
 import { OpenAIEmbeddings } from '@langchain/openai';
 
 export const embeddings = new OpenAIEmbeddings({
-  model: 'text-embedding-3-small',
+  model: 'text-embedding-3-large',
   openAIApiKey: process.env.OPENAI_API_KEY,
 });
