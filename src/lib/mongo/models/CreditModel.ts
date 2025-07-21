bab70980b8f43061b192cf5d1e2faed4e888b030
Simas <simaszurauskas@gmail.com>
2025-07-21 22:40:42 +0300

    Update default credit value in CreditModel to 10

--- src/lib/mongo/models/CreditModel.ts
+++ src/lib/mongo/models/CreditModel.ts
@@ -15,7 +15,7 @@ export interface Credit extends CreditInput, Document {
 const creditSchema = new Schema<Credit>(
   {
     email: { type: String, required: true, unique: true },
-    value: { type: Number, default: 15 },
+    value: { type: Number, default: 10 },
   },
   {
     timestamps: true,
